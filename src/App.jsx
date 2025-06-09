db3d4c6e186a39e2d8d98c30b304bb6e4eee9b38
panishthaa <panishthaa@gmail.com>
2025-06-10 03:12:24 +0530

    error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import styles from  "./App.module.css";
-import { Navbar } from "./components/navbar/navbar";
-import { Hero } from "./components/Hero/hero";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Hero } from "./components/Hero/Hero";
 import { About } from "./components/About/About";
 import { Experience } from "./components/Experience/Experience";
 import {Projects} from "./components/Projects/Projects";
