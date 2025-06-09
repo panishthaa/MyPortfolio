62af67ae119357182a6c11ef5640fc9d04902684
panishthaa <panishthaa@gmail.com>
2025-06-10 03:08:03 +0530

    error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import styles from  "./App.module.css";
 import { Navbar } from "./components/navbar/navbar";
 import { Hero } from "./components/Hero/hero";
-import { About } from "./components/about/About";
+import { About } from "./components/About/About";
 import { Experience } from "./components/Experience/Experience";
 import {Projects} from "./components/Projects/Projects";
 import { Contact } from "./components/Contact/Contact";
